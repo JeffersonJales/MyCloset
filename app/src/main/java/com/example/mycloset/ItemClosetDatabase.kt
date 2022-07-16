bf44bae15a3068dc6c4f4c853fea92584c595461
JeffersonJales <jeffersonjales@hotmail.com>
2022-07-16 18:19:29 -0300

    My List Itens

--- app/src/main/java/com/example/mycloset/ItemClosetDatabase.kt
+++ app/src/main/java/com/example/mycloset/ItemClosetDatabase.kt
@@ -2,11 +2,13 @@ package com.example.mycloset
 
 import java.io.Serializable
 
-data class ItemCloset(
+open class ItemCloset(
     var nome : String = "",
     var preco_compra : String = "",
     var preco_venda : String = "",
     var foto : String = "",
     var categoria : String = "",
     var descricao : String = ""
-) : Serializable
+)
+
+class ItemClosetId(var id : String = "", var obj : ItemCloset = ItemCloset())
--- app/src/main/java/com/example/mycloset/ListaitemsClosetActivity.kt
+++ app/src/main/java/com/example/mycloset/ListaitemsClosetActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mycloset
 
+import adapters.CardItemClosetAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -7,11 +8,14 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.getValue
+import com.google.firebase.storage.FirebaseStorage
 
 class ListaitemsClosetActivity : AppCompatActivity() {
 
@@ -25,6 +29,41 @@ class ListaitemsClosetActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_item_closet)
 
+
+
+        /// Recicle Viewer
+        val userPrefs  = UserPreferences(applicationContext)
+        var userInfo = userPrefs.LoadPreferences()
+        if(userInfo.uid != ""){
+            val recyclerview = findViewById<RecyclerView>(R.id.rcItemsList)
+            recyclerview.layoutManager = LinearLayoutManager(this)
+
+            var arrListIds : ArrayList<String> = ArrayList(0)
+
+            var evaluate = object : ValueEventListener{
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(!snapshot.exists()) return
+
+                    for( ids in snapshot.children ){
+                        arrListIds.add( ids.value.toString() )
+                    }
+
+                    val adapter = CardItemClosetAdapter(arrListIds)
+                    recyclerview.adapter = adapter
+                }
+            }
+
+            fDatabase.reference.
+                child("usuarios").
+                child(userInfo.uid).
+                child("items_closet").
+                addValueEventListener(evaluate)
+        }
+
+        /// Botoes
         btListarItemsChecarCodigo = findViewById(R.id.btListaItemChecarCodigo)
         etListarItemsInserirCodigo = findViewById(R.id.etListaItemInserirCodigo)
     }
